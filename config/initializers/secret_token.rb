c6712164d7489a906706f2de701b76db852cb308
Abhas Arya <abhasarya@abhass-mbp.home>
2015-05-23 14:05:48 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WecookBeta::Application.config.secret_key_base = '91fdc61c33b637de7483db2ae1fe359557c2d9b2d33e4fec780e5c030f899807f9947a746c6731cf4e2d2338d70951594bf1b044bbfcf771eb37be9de5c9fd6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WecookBeta::Application.config.session_store :cookie_store, key: '_wecook_beta_session'
